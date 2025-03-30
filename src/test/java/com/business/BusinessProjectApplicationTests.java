92df379d90e614b62b68e28d535a029e5822b5b8
Babrubahan123 <babru89.samal@gmail.com>
2025-03-30 21:25:29 +0530

    initial commit

---
+++ src/test/java/com/business/BusinessProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.business;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessProjectApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
